93864b0869c875e9c45024f8b0c77f42ac85eba4
anyoshe <anyokaayoka@gmail.com>
2024-10-14 12:20:43 +0300

    Initial commit with backend and client folders

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import EmployerLandingPage from './Components/LandingPage/JobsLandingpage/EmployerLandingPage.js';
 import AuthForm from './Components/LandingPage/SignUpLogin/AuthForm.js';
 import EmployerAuthForm from './Components/LandingPage/SignUpLogin/EmployerAuthForm.js';
+import JobSeekerLandingPage from './Components/LandingPage/JobSeekerLandingPage/JobSeeker.js';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
      <AuthForm />
      <EmployerAuthForm />
      <EmployerLandingPage />
+     <JobSeekerLandingPage />
    </div>
   );
 }
